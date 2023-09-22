dff2bb3a95021ee6dc15b94c6a2b192b7500c573
Nguyễn Bùi Quốc Huy <118173923+nbqhuy@users.noreply.github.com>
2023-09-22 21:19:34 +0700

    add animation

--- src/components/CardContainer.style.jsx
+++ src/components/CardContainer.style.jsx
@@ -1,14 +1,24 @@
-import styled from "styled-components"
+import styled, {keyframes} from "styled-components";
+
+const gradient  = keyframes`
+    0%{background-position:0% 67%}
+    50%{background-position:100% 34%}
+    100%{background-position:0% 67%}
+`;
+
 export const CardTomorrowContainerStyle = styled.div`
-    width: 100%;
-    height: 25%;
-    background: linear-gradient(170deg, rgba(9,9,121,1) 0%, rgba(9,9,121,1) 25%, rgba(0,212,255,1) 100%);
-    border-radius: 30px;
-    display: flex;
-    justify-content: space-around;
-`
+  width: 100%;
+  height: 25%;
+  background: linear-gradient(201deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+  background-size: 400% 400%;
+  animation: ${gradient} 30s ease infinite;
+  border-radius: 30px;
+  display: flex;
+  justify-content: space-around;
+`;
+
 export const CardDailyContainerStyle = styled(CardTomorrowContainerStyle)`
-    height: 30%;
-    flex-direction: column;
-    padding: 3% 0;
-`
+  height: 30%;
+  flex-direction: column;
+  padding: 3% 0;
+`;
